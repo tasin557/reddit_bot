d0922fae40980f5dbdbd7407c87f26b23894e341
Gazi Tasin Rahman <96831022+tasin557@users.noreply.github.com>
2025-03-02 14:09:02 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "Ok_Gold_407"
-password = "sdor24e9n"
-client_id = "N10hm8zzLBxwsETYTIy42Q"
-client_secret = "7ukhugvvGgKgD9gtHmSIRwh2I7lF3g"
+username = "*************"
+password = "**********"
+client_id = "*****************"
+client_secret = "****************"
